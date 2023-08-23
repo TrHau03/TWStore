961d4e2794ef0eb70e5883d76e842aa393969d38
TrHau03 <haunhoooo@gmail.com>
2023-08-23 20:28:47 +0700

    UpdateScreen

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,85 +1,205 @@
-import { StyleSheet, Text, View, ScrollView, Image, Pressable } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, FlatList, ListRenderItemInfo, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
-import { Stepper } from '@ant-design/react-native'
-const CartScreen = () => {
-    const [numberCount, setNumberCount] = useState<number>(1)
+import { InputItem, Stepper } from '@ant-design/react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import LinearGradient from 'react-native-linear-gradient';
+interface Product {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+    size: string;
+    color: string;
+    evalute: number;
+    description: string;
+    type: string;
+}
+
+
+const RenderItem = ({ item }: { item: Product }) => {
+    const [numberCount, setNumberCount] = useState<number>(1);
     return (
-        <View style={{ paddingHorizontal: 16 }}>
-            <View style={{ marginTop: 20 }}>
+        <View style={styles.itemCart}>
+            <View>
+                <Image source={{ uri: item.image }} style={{ width: 72, height: 72 }} />
+            </View>
+            <View style={{ flexDirection: 'column', height: '100%' }}>
+                <View style={styles.topItem}>
+                    <Text style={styles.textTitleItem}>{item.name}</Text>
+                    <Icon name='heart' size={25} color={'#FB7181'} />
+                    <Icon name='trash-outline' size={25} />
+                </View>
+                <View style={styles.bottomItem}>
+                    <Text style={styles.textPrice}>${item.price}</Text>
+                    <View style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', justifyContent: 'space-between', width: 100, height: 30, paddingHorizontal: 2, position: 'absolute', right: 30 }}>
+                        <Pressable onPress={() => numberCount > 1 ? setNumberCount(numberCount - 1) : setNumberCount(1)} style={numberCount > 1 ? styles.btnNumberCountMinus : [styles.btnNumberCountMinus, { backgroundColor: '#E5E5E5' }]}><Icon name='remove-outline' size={25} /></Pressable>
+                        <Text style={styles.textNumberCount}>{numberCount}</Text>
+                        <Pressable onPress={() => numberCount < 10 ? setNumberCount(numberCount + 1) : setNumberCount(10)} style={numberCount < 10 ? styles.btnNumberCountPlus : [styles.btnNumberCountPlus, { backgroundColor: '#E5E5E5' }]}><Icon name='add-outline' size={25} /></Pressable>
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+
+const CartScreen = () => {
+    const [cupon, setCupon] = useState<string>('');
+    return (
+        <KeyboardAwareScrollView style={{ paddingHorizontal: 16 }} scrollEnabled={false}>
+            <View style={{ marginTop: 17 }}>
                 <Text style={styles.txtTitlePage}>Your Cart</Text>
             </View>
             <View style={styles.line}></View>
-            <ScrollView style={{ height: '30%', marginTop: '20%'}}>
-                <View style={styles.itemCart}>
-                    <View>
-                        <Image source={require('../asset/image/product_1.png')} />
-                    </View>
-                    <View style={{ flexDirection: 'column', height: '100%' }}>
-                        <View style={{ height: '50%', flexDirection: 'row', alignItems: 'center', gap: 10, paddingLeft: 20 }}>
-                            <Text style={styles.textTitleItem}>Nike Air Zoom Pegasus 36 Miami</Text>
-                            <Icon name='heart' size={25} color={'#FB7181'} />
-                            <Icon name='trash-outline' size={25} />
-                        </View>
-                        <View style={{ flexDirection: 'row', height: '50%', alignItems: 'center', paddingLeft: 20, gap: 100 }}>
-                            <Text style={styles.textPrice}>$299,43</Text>
-                            <View style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', justifyContent: 'space-between', width: 100, height: 30, paddingHorizontal: 2 }}>
-                                <Pressable onPress={() => numberCount > 1 ? setNumberCount(numberCount - 1) : setNumberCount(1)} style={numberCount > 1 ? styles.btnNumberCountMinus : [styles.btnNumberCountMinus, { backgroundColor: '#E5E5E5' }]}><Icon name='remove-outline' size={25} /></Pressable>
-                                <Text style={styles.textNumberCount}>{numberCount}</Text>
-                                <Pressable onPress={() => numberCount < 10 ? setNumberCount(numberCount + 1) : setNumberCount(10)} style={numberCount < 10 ? styles.btnNumberCountPlus : [styles.btnNumberCountPlus, { backgroundColor: '#E5E5E5' }]}><Icon name='add-outline' size={25} /></Pressable>
-                            </View>
-                        </View>
-                    </View>
+            <FlatList style={{ maxHeight: '25%', marginTop: '11%' }} showsVerticalScrollIndicator={false}
+                renderItem={(object) => <RenderItem item={object.item} />}
+                data={data}
+                keyExtractor={(item: Product) => item.id.toString()}
+            />
+            <View style={{ borderWidth: 1, borderColor: '#9098B1', borderRadius: 5, marginTop: 25 }}>
+                <InputItem
+                    style={{ fontSize: 16 }}
+                    value={cupon}
+                    onChange={(value: any) => {
+                        setCupon(value)
+                    }}
+                    placeholder="Enter Cupon Code"
+                    extra={
+                        <Pressable style={styles.btnApply}>
+                            <Text style={styles.textApply}>Apply</Text>
+                        </Pressable>
+                    }
+                />
+            </View>
+            <View style={styles.itemTotalPrice}>
+                <View style={styles.headerTotalPrice}>
+                    <Text style={styles.textHeaderTotalLeft}>Items (3)</Text>
+                    <Text style={styles.textHeaderTotalRight}>$598.86</Text>
                 </View>
-                <View style={styles.itemCart}>
-                    <View>
-                        <Image source={require('../asset/image/product_1.png')} />
-                    </View>
-                    <View style={{ flexDirection: 'column', height: '100%' }}>
-                        <View style={{ height: '50%', flexDirection: 'row', alignItems: 'center', gap: 10, paddingLeft: 20 }}>
-                            <Text style={styles.textTitleItem}>Nike Air Zoom Pegasus 36 Miami</Text>
-                            <Icon name='heart' size={25} color={'#FB7181'} />
-                            <Icon name='trash-outline' size={25} />
-                        </View>
-                        <View style={{ flexDirection: 'row', height: '50%', alignItems: 'center', paddingLeft: 20, gap: 100 }}>
-                            <Text style={styles.textPrice}>$299,43</Text>
-                            <View style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', justifyContent: 'space-between', width: 100, height: 30, paddingHorizontal: 2 }}>
-                                <Pressable onPress={() => numberCount > 1 ? setNumberCount(numberCount - 1) : setNumberCount(1)} style={numberCount > 1 ? styles.btnNumberCountMinus : [styles.btnNumberCountMinus, { backgroundColor: '#E5E5E5' }]}><Icon name='remove-outline' size={25} /></Pressable>
-                                <Text style={styles.textNumberCount}>{numberCount}</Text>
-                                <Pressable onPress={() => numberCount < 10 ? setNumberCount(numberCount + 1) : setNumberCount(10)} style={numberCount < 10 ? styles.btnNumberCountPlus : [styles.btnNumberCountPlus, { backgroundColor: '#E5E5E5' }]}><Icon name='add-outline' size={25} /></Pressable>
-                            </View>
-                        </View>
-                    </View>
+                <View style={styles.headerTotalPrice}>
+                    <Text style={styles.textHeaderTotalLeft}>Items (3)</Text>
+                    <Text style={styles.textHeaderTotalRight}>$598.86</Text>
                 </View>
-                <View style={styles.itemCart}>
-                    <View>
-                        <Image source={require('../asset/image/product_1.png')} />
-                    </View>
-                    <View style={{ flexDirection: 'column', height: '100%' }}>
-                        <View style={{ height: '50%', flexDirection: 'row', alignItems: 'center', gap: 10, paddingLeft: 20 }}>
-                            <Text style={styles.textTitleItem}>Nike Air Zoom Pegasus 36 Miami</Text>
-                            <Icon name='heart' size={25} color={'#FB7181'} />
-                            <Icon name='trash-outline' size={25} />
-                        </View>
-                        <View style={{ flexDirection: 'row', height: '50%', alignItems: 'center', paddingLeft: 20, gap: 100 }}>
-                            <Text style={styles.textPrice}>$299,43</Text>
-                            <View style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 5, alignItems: 'center', justifyContent: 'space-between', width: 100, height: 30, paddingHorizontal: 2 }}>
-                                <Pressable onPress={() => numberCount > 1 ? setNumberCount(numberCount - 1) : setNumberCount(1)} style={numberCount > 1 ? styles.btnNumberCountMinus : [styles.btnNumberCountMinus, { backgroundColor: '#E5E5E5' }]}><Icon name='remove-outline' size={25} /></Pressable>
-                                <Text style={styles.textNumberCount}>{numberCount}</Text>
-                                <Pressable onPress={() => numberCount < 10 ? setNumberCount(numberCount + 1) : setNumberCount(10)} style={numberCount < 10 ? styles.btnNumberCountPlus : [styles.btnNumberCountPlus, { backgroundColor: '#E5E5E5' }]}><Icon name='add-outline' size={25} /></Pressable>
-                            </View>
-                        </View>
-                    </View>
+                <View style={styles.headerTotalPrice}>
+                    <Text style={styles.textHeaderTotalLeft}>Items (3)</Text>
+                    <Text style={styles.textHeaderTotalRight}>$598.86</Text>
                 </View>
-            </ScrollView >
-
-        </View >
+                <View style={styles.bottomTotalPrice}>
+                    <Text style={styles.textBottomTotalLeft}>Items (3)</Text>
+                    <Text style={styles.textBottomTotalRight}>$598.86</Text>
+                </View>
+            </View>
+            <View>
+                <TouchableOpacity >
+                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={['#46caf3', '#5cbae3', '#68b1d9']} style={styles.btnCheckOut} >
+                        <Text style={styles.textCheckOut}>Login</Text>
+                    </LinearGradient>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAwareScrollView >
     )
 }
 
 export default CartScreen
 
 const styles = StyleSheet.create({
+    btnCheckOut: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 55,
+        borderRadius: 5,
+        marginTop: 34
+      },
+    textCheckOut: {
+        color: 'white',
+        fontSize: 24,
+        fontFamily: 'Poppins',
+        fontWeight: '700',
+      },
+    textBottomTotalRight: {
+        color: '#40BFFF',
+        fontSize: 14,
+        fontFamily: 'Poppins',
+        fontWeight: '700',
+        lineHeight: 18,
+        letterSpacing: 0.50,
+    },
+    textBottomTotalLeft: {
+        color: '#223263',
+        fontSize: 14,
+        fontFamily: 'Poppins',
+        fontWeight: '700',
+        lineHeight: 18,
+        letterSpacing: 0.50,
+    },
+    textHeaderTotalRight: {
+        color: '#223263',
+        fontSize: 14,
+        fontFamily: 'Poppins',
+        fontWeight: '400',
+        lineHeight: 21.60,
+        letterSpacing: 0.50,
+    },
+    textHeaderTotalLeft: {
+        color: '#9098B1',
+        fontSize: 14,
+        fontFamily: 'Poppins',
+        fontWeight: '400',
+        letterSpacing: 0.50,
+    },
+    bottomTotalPrice: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10,
+        borderTopWidth: 0.5,
+        borderColor: '#9098B1',
+        alignItems: 'center',
+    },
+    headerTotalPrice: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    itemTotalPrice: {
+        padding: 16,
+        borderWidth: 0.5,
+        borderColor: '#9098B1',
+        borderRadius: 5,
+        marginTop: 20
+    },
+    btnApply: {
+        backgroundColor: '#40BFFF',
+        width: 65,
+        height: '101%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        right: -16,
+        borderBottomRightRadius: 5,
+        borderTopRightRadius: 5
+    },
+    textApply: {
+        color: 'white',
+        fontSize: 14,
+        fontFamily: 'Poppins',
+        fontWeight: '700',
+        lineHeight: 21.60,
+        letterSpacing: 0.50,
+    },
+    topItem: {
+        height: '50%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+        paddingLeft: 20
+    },
+    bottomItem: {
+        flexDirection: 'row',
+        height: '50%',
+        alignItems: 'center',
+        paddingLeft: 20,
+    },
     btnNumberCountMinus: {
         backgroundColor: '#EBF0FF',
         borderTopStartRadius: 5,
@@ -131,7 +251,7 @@ const styles = StyleSheet.create({
         width: '120%',
         height: 1,
         backgroundColor: '#E5E5E5',
-        marginTop: 80,
+        marginTop: 60,
 
     },
     txtTitlePage: {
@@ -143,3 +263,54 @@ const styles = StyleSheet.create({
         letterSpacing: 0.08,
     }
 })
+const data = [{
+    "id": 1,
+    "name": "Mediterranean Brome",
+    "image": "http://dummyimage.com/72x72.png/dddddd/000000",
+    "price": 1999,
+    "size": "XS",
+    "color": "#b2e162",
+    "evalute": 5,
+    "description": "Late ef-spch/lang df NEC",
+    "type": "MULTI VITAMIN INFUSION"
+}, {
+    "id": 2,
+    "name": "Bellflower",
+    "image": "http://dummyimage.com/72x72.png/dddddd/000000",
+    "price": 259,
+    "size": "M",
+    "color": "#53918b",
+    "evalute": 2,
+    "description": "Budd-chiari syndrome",
+    "type": "Lamotrigine"
+}, {
+    "id": 3,
+    "name": "Hollyhock",
+    "image": "http://dummyimage.com/72x72.png/dddddd/000000",
+    "price": 99,
+    "size": "3XL",
+    "color": "#f5c92e",
+    "evalute": 1,
+    "description": "Blepharochalasis",
+    "type": "Pravastatin Sodium"
+}, {
+    "id": 4,
+    "name": "Dock",
+    "image": "http://dummyimage.com/72x72.png/ff4444/ffffff",
+    "price": 399,
+    "size": "M",
+    "color": "#fde3b6",
+    "evalute": 1,
+    "description": "Special symptom NEC/NOS",
+    "type": "Ablavar"
+}, {
+    "id": 5,
+    "name": "Pink Mountainheath",
+    "image": "http://dummyimage.com/72x72.png/ff4444/ffffff",
+    "price": 200,
+    "size": "3XL",
+    "color": "#330c8f",
+    "evalute": 4,
+    "description": "Mucous polyp of cervix",
+    "type": "Prednisone"
+}]
