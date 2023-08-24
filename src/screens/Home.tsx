70ea847ebaf37e6672c1cde3853ed47ee60680ae
Phuc-T <thiennpps20462@fpt.edu.vn>
2023-08-24 13:19:54 +0700

    ok la

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, Image, Pressable, ScrollView, FlatList } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Image, Pressable, ScrollView, FlatList, SectionList  } from 'react-native'
 import React, { useState } from 'react'
 
 const images = [
@@ -12,7 +12,7 @@ const Home = () => {
 
   const [imgActive, setimgActive] = useState(0);
 
-  const onChange = (nativeEvent) => {
+  const onChange = (nativeEvent: any) => {
     if (nativeEvent) {
       const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
       if (slide >= 0) {
@@ -29,6 +29,12 @@ const Home = () => {
     { id: '5', name: 'Man Shoes', icon: require('../asset/img/manshoes.png') },
   ];
 
+  const data2 = [
+    { id: '1', name: 'FS - Nike Air Max 270 React...', image: require('../asset/img/giay.png') },
+    { id: '2', name: 'FE - QUILTED MAXI CROS...', image: require('../asset/img/giay.png') },
+    { id: '3', name: 'FA - Nike Air Max 350 React...', image: require('../asset/img/giay.png') },
+  ];
+
   const renderItem = ({ item }: { item: { id: string, name: string, icon: any } }) => (
     <View style={styles.item}>
       <View style={styles.bodericon}>
@@ -38,8 +44,34 @@ const Home = () => {
     </View>
   );
 
+  const renderItem2 = ({ item }: { item: { id: string, name: string, image: any } }) => (
+    <View style={styles.itemsale}>
+      <Image style={styles.imageproduct} source={item.image} />
+      <Text style={styles.nameproduct}>{item.name}</Text>
+
+      <Text style={styles.price}>$299,43</Text>
+      <View style={styles.stylesaleoff}>
+        <Text style={styles.strikethrough}>$534,33</Text>
+        <Text style={styles.saleoff}>24% Off</Text>
+      </View>
+    </View>
+  );
+
+  const renderItem3 = ({ item }: { item: { id: string, name: string, image: any } }) => (
+    <View style={styles.itemsale2}>
+      <Image style={styles.imageproduct} source={item.image} />
+      <Text style={styles.nameproduct}>{item.name}</Text>
+      <Image style={styles.imga} source={require('../asset/img/a.png')} />
+      <Text style={styles.price}>$299,43</Text>
+      <View style={styles.stylesaleoff}>
+        <Text style={styles.strikethrough}>$534,33</Text>
+        <Text style={styles.saleoff}>24% Off</Text>
+      </View>
+    </View>
+  );
+
   return (
-    <View>
+    <ScrollView>
 
       <View style={styles.top}>
 
@@ -64,6 +96,7 @@ const Home = () => {
       <View style={styles.topslide}>
 
         <ScrollView
+          nestedScrollEnabled = {true}
           onScroll={({ nativeEvent }) => onChange(nativeEvent)}
           showsHorizontalScrollIndicator={false}
           pagingEnabled
@@ -116,7 +149,58 @@ const Home = () => {
         />
       </View>
 
-    </View>
+      <View style={styles.flashsale}>
+
+        <Text style={styles.textflashsale}>Flash Sale</Text>
+
+        <Pressable>
+          <Text style={styles.textflashsale2}>
+            See More
+          </Text>
+        </Pressable>
+
+      </View>
+
+      <View style={styles.listflastsale}>
+        <FlatList
+          data={data2}
+          horizontal
+          renderItem={renderItem2}
+          keyExtractor={(item) => item.name}
+        />
+      </View>
+
+      <View style={styles.megasale}>
+        <Text style={styles.textflashsale}>Mega Sale</Text>
+        <Pressable>
+          <Text style={styles.textflashsale2}>
+            See More
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.listflastsale}>
+        <FlatList
+          data={data2}
+          horizontal
+          renderItem={renderItem2}
+          keyExtractor={(item) => item.name}
+        />
+      </View>
+
+      <Image style={styles.imgrecomended} source={require('../asset/img/r.png')} />
+
+      <View style={styles.listgrid}>
+        <FlatList
+          data={data2}
+          renderItem={renderItem3}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          columnWrapperStyle={styles.columnWrapper}
+        />
+      </View>
+
+    </ScrollView>
   )
 }
 
@@ -204,18 +288,18 @@ const styles = StyleSheet.create({
   },
 
   listcategory: {
-    height: 110,
+    height: 100,
     marginLeft: 20,
   },
 
   item: {
-    marginRight: 22 ,
+    marginRight: 22,
     alignItems: 'center',
   },
 
   Icon: {
-    width: 26 ,
-    height: 26 ,
+    width: 26,
+    height: 26,
   },
 
   textname: {
@@ -223,13 +307,122 @@ const styles = StyleSheet.create({
   },
 
   bodericon: {
-    width: 60 ,
-    height: 60 ,
-    borderWidth: 1 ,
-    borderRadius: 90 ,
+    width: 60,
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 90,
     borderColor: '#EBF0FF',
     alignItems: 'center',
-    justifyContent: 'center' ,
+    justifyContent: 'center',
+  },
+
+  flashsale: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+  },
+
+  textflashsale: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#BF0202'
+  },
+
+  textflashsale2: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: 'black'
+  },
+
+  listflastsale: {
+    height: 240,
+    marginLeft: 20,
+    marginTop: 15,
+  },
+
+  itemsale: {
+    height: 240,
+    width: 140,
+    marginRight: 9,
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: '#EBF0FF',
+    justifyContent: 'space-around',
+  },
+
+  imageproduct: {
+    marginLeft: 15
+  },
+
+  nameproduct: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    color: '#223263',
+    width: 110,
+    marginLeft: 15
+  },
+
+  price: {
+    fontWeight: 'bold',
+    fontSize: 13,
+    color: '#4464C4',
+    marginLeft: 15
+  },
+
+  stylesaleoff: {
+    flexDirection: 'row'
+  },
+
+  strikethrough: {
+    textDecorationLine: 'line-through',
+    fontSize: 10,
+    marginRight: 12,
+    marginLeft: 15
+  },
+
+  saleoff: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    color: '#FB7181'
+  },
+
+  megasale: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10
+  },
+
+  imgrecomended: {
+    marginTop: 10,
+    marginLeft: 20 ,
+  },
+
+  imga: {
+    marginLeft: 15
+  },
+
+  columnWrapper: {
+    justifyContent: 'flex-start',
+  },
+
+  listgrid: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+
+  itemsale2: {
+    height: 240,
+    width: 165,
+    marginRight: 9,
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: '#EBF0FF',
+    justifyContent: 'space-around',
   }
 
 })
