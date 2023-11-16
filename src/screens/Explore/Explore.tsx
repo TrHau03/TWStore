7f2386740f51cbc399a71d3e326406e06bd5779c
Minhld-21 <100395104+Minhld-21@users.noreply.github.com>
2023-11-16 18:03:00 +0700

    minh dep trai 16/11

--- src/screens/Explore/Explore.tsx
+++ src/screens/Explore/Explore.tsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   FlatList,
   Image,
   StyleSheet,
@@ -7,30 +8,46 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { CompositeNavigationProp, CompositeScreenProps, useNavigation } from '@react-navigation/native';
 import { RootStackParamListHome, RootStackScreenEnumHome } from '../../component/Root/RootStackHome';
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 import { BottomTabNavigationProp, BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { RootTabParamList, RootTabScreenENum } from '../../component/BottomNavigation/RootTab/RootTab';
-
-
+import { SelectList } from 'react-native-dropdown-select-list';
+import { useDispatch, useSelector } from 'react-redux';
+import { listRecommendeds, todoRemainingRecomendeds } from '../../redux/silces/HomeSelector';
+import HomeScreenSlice from '../../redux/silces/HomeScreenSlice';
 
 interface Category {
   id: number;
   img: any;
   name: string;
 }
-type ProfileScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<RootTabParamList, 'StackHome'>,StackNavigationProp<RootStackParamListHome, RootStackScreenEnumHome>>;
+type ProfileScreenNavigationProp = CompositeNavigationProp<BottomTabNavigationProp<RootTabParamList, 'StackHome'>, StackNavigationProp<RootStackParamListHome, RootStackScreenEnumHome>>;
 const ExploreScreen = () => {
   const navigation = useNavigation<ProfileScreenNavigationProp>();
+  const textInputRef = useRef(null);
+
 
   const [click, setClick] = useState<boolean>(false);
+  console.log(click);
+  //redux
+  const todoList = useSelector(todoRemainingRecomendeds);
+  const [textInputSearch, setTextInputSearch] = useState<string>('');
+  const dispatch = useDispatch();
 
+  const handleSearch = (e: any) => {
+    setTextInputSearch(e);
+    dispatch(
+      HomeScreenSlice.actions.searchFilterChange(e)
+    )
+  }
 
   const renderItem = ({ item }: any): React.JSX.Element => {
     const { id, img, name } = item;
+
 
     return (
       <TouchableOpacity
@@ -45,25 +62,41 @@ const ExploreScreen = () => {
             <Text style={styles.NamePD}>{name}</Text>
           </View>
         </View>
-
       </TouchableOpacity>
     );
   };
+
+  const renderItemSearch = ({ item }: any): React.JSX.Element => {
+    return (
+      <View style={{ padding: 10 }}>
+        <Text style={styles.TextSearch}>{item.name}</Text>
+      </View>
+    )
+  }
 
 
 
   return (
     <View style={styles.container} >
       <View style={styles.group}>
-        <View style={(!click) ? styles.headerLeft : [styles.headerLeft, { borderColor: 'blue' }]}
-        >
-          <Icon name='search' size={22} />
+
+        <View style={(!click) ? styles.headerLeft : [styles.headerLeft, { borderColor: 'blue' }]}>
+          <View style={{ width: '10%' }} >
+            <Icon name='search' size={22} />
+          </View>
           <TextInput
             placeholder="Search here"
             style={styles.TextSearch}
-            onFocus={() => setClick(true)}
-            onBlur={() => setClick(false)}
+            onFocus={() => setClick(!click)}
+            onBlur={() => setClick(!click)}
+            onChangeText={handleSearch}
+            value={textInputSearch}
+            ref={textInputRef}
           />
+
+          <TouchableOpacity style={{ width: '10%' }} onPress={() => {setClick(false); setTextInputSearch('')}}>
+            <Icon name='close-outline' size={22} />
+          </TouchableOpacity>
         </View>
         <View style={styles.headerRight}>
           <TouchableOpacity onPress={() => navigation.navigate(RootStackScreenEnumHome.FavoriteScreen)}>
@@ -74,27 +107,40 @@ const ExploreScreen = () => {
           </TouchableOpacity>
         </View>
       </View>
-      <View style={styles.Name}>
-        <Text style={styles.txtName}>Man Fashion</Text>
-        <FlatList
-          data={DataMan}
-          renderItem={renderItem}
-          keyExtractor={item => item.id.toString()}
-          numColumns={4}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
+      {(click) ?
+        <View>
+          <FlatList
+            renderItem={renderItemSearch}
+            data={textInputSearch == "" ? null : todoList}
+            style={{ paddingVertical: 15, height: '100%', width: '100%' }}
+          />
+        </View>
+        :
+        <View>
+          <View style={styles.Name}>
+            <Text style={styles.txtName}>Man Fashion</Text>
+            <FlatList
+              data={DataMan}
+              renderItem={renderItem}
+              keyExtractor={item => item.id.toString()}
+              numColumns={4}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
 
-      <View style={styles.Name}>
-        <Text style={styles.txtName}>Woman Fashion</Text>
-        <FlatList
-          data={DataWoman}
-          renderItem={renderItem}
-          keyExtractor={item => item.id.toString()}
-          numColumns={4}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
+          <View style={styles.Name}>
+            <Text style={styles.txtName}>Woman Fashion</Text>
+            <FlatList
+              data={DataWoman}
+              renderItem={renderItem}
+              keyExtractor={item => item.id.toString()}
+              numColumns={4}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      }
+
 
     </View>
 
@@ -139,8 +185,7 @@ const styles = StyleSheet.create({
 
 
   TextSearch: {
-    justifyContent: 'center',
-    marginLeft: 20
+    width: '80%',
   },
   imageSearch: {
     width: 20,
@@ -162,17 +207,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     width: '80%',
-    height: '100%'
+    height: '100%',
   },
   group: {
     flexDirection: 'row',
     width: '100%',
     height: 50,
-
   },
   container: {
     paddingTop: 10,
-    flex: 1,
     paddingHorizontal: 15,
     backgroundColor: '#fff'
   }
@@ -243,3 +286,7 @@ const DataWoman: Category[] = [
     name: 'Red Apple',
   },
 ];
+function dispatch(arg0: { payload: any; type: "HomeScreenSlice/searchFilterChange"; }) {
+  throw new Error('Function not implemented.');
+}
+
