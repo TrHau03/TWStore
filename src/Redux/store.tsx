5854305e307d6b961cb7df1b4d15f365ae7142a6
TrHau03 <haunhoooo@gmail.com>
2023-11-24 23:05:01 +0700

    update

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,10 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit'
-import filtersSlice from '../screens/Explore/Slice/filtersSlice'
-import todosSlice from '../screens/Explore/Slice/todosSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import Slides from './silces/Silces';
+import CartSlices from './silces/CartSlices';
+import HomeScreenSlice from './silces/HomeScreenSlice';
 const store = configureStore({
-    reducer:{
-        filters: filtersSlice.reducer,
-        todoList:todosSlice.reducer, 
+    reducer: {
+        SlicesReducer: Slides,
+        CartReducer: CartSlices,
+        HomeScreenSlice: HomeScreenSlice.reducer,
     }
 })
 export default store;
